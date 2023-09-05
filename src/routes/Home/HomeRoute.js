a7c2fcac6883ea0204790ed5635d47fce3386694
CYNTAX <cyntax@CYNTAXs-MacBook-Pro.local>
2023-09-05 14:10:58 +0100

    new branch

---
+++ src/routes/Home/HomeRoute.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "../../components/Home/Home";
+const HomeRoute = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default HomeRoute;
