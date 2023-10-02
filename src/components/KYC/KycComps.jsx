adcae8b27d75be493e15b5518d60c0193ca3b3f7
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-10-02 15:14:47 +0100

    added the disbursement route

--- src/components/KYC/KycComps.jsx
+++ src/components/KYC/KycComps.jsx
@@ -16,11 +16,17 @@ import Webcam from "react-webcam";
 
 import axios from "axios";
 import { dataUrlToFile } from "../../utils/Base64ToFile";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const levels = Object.freeze({
   level1: "LEVEL_1",
   level2: "LEVEL_2",
   level3: "LEVEL_3",
+});
+const status = Object.freeze({
+  verified: "VERIFIED",
+  notVerified: "NOT_VERIFIED",
 });
 const styles = {
   container: {
@@ -52,18 +58,30 @@ const KycEmailComp = ({ toggleEmailCont }) => {
   const [response, setResponse] = useState({});
   const fetchKycStatus = async () => {
     const response = await GET_KYC_STATUS();
+    console.log(response);
 
     if (response?.code === null || !response?.code === 200) {
       return;
     }
 
+    // if (response.data.data.status === status.verified) {
+    //   toggleEmailCont();
+    //   return;
+    // }
+
     if (
-      response.data.data.level === levels.level1 ||
-      response.data.data.level === levels.level2 ||
-      response.data.data.level === levels.level3
+      (response.data.data.level === levels.level1 &&
+        response.data.data.status === status.verified) ||
+      (response.data.data.level === levels.level2 &&
+        response.data.data.status === status.notVerified) ||
+      (response.data.data.level === levels.level2 &&
+        response.data.data.status === status.verified) ||
+      (response.data.data.level === levels.level3 &&
+        response.data.data.status === status.notVerified) ||
+      (response.data.data.level === levels.level3 &&
+        response.data.data.status === status.verified)
     ) {
       toggleEmailCont();
-      return;
     }
     setResponse(response?.data?.data);
     console.log(response);
@@ -106,7 +124,7 @@ const KycEmailComp = ({ toggleEmailCont }) => {
             </div>
           </div>
         </div>
-        {!response.level === levels.level1 && (
+        {/* {!response?.level === levels.level1 && (
           <div className="kypageDiv_cont_button_div">
             <button
               className="kypageDiv_cont_button_div_btn"
@@ -115,85 +133,122 @@ const KycEmailComp = ({ toggleEmailCont }) => {
               Continue
             </button>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
 };
 const KycStartComp = ({ startVerify, prev }) => {
+  const [response, setResponse] = useState({});
+  const fetchKycStatus = async () => {
+    const response = await GET_KYC_STATUS();
+    console.log(response);
+
+    if (response?.code === null || !response?.code === 200) {
+      return;
+    }
+
+    if (
+      response.data?.data?.level === levels.level2 &&
+      response.data.data.status === status.notVerified
+    ) {
+      // nextStep1();
+    }
+    setResponse(response?.data?.data);
+  };
+
+  useEffect(() => {
+    fetchKycStatus();
+  }, []);
+
+  if (
+    response.level === levels.level2 &&
+    response.status === status.notVerified
+  ) {
+    return (
+      <div className="kypageDiv_cont_div">
+        <div className="kypageDiv_cont_div_btn">
+          <h1>Your Document is being reviewied </h1>
+          <p>Once completed, you'll be notified</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="kypageDiv_cont_div">
-      <div className="kypageDiv_cont_div_btn">
-        <div className="kypageDiv_cont_div_btn_func_btn" onClick={prev}>
-          <ArrowBackIcon className="kypageDiv_cont_div_btn_func_btn_icon" />
-          Home
-        </div>
-        Verify
-      </div>
-      <div className="kypageDiv_cont">
-        <div className="kypageDiv_cont_body_conts">
-          <div className="kypageDiv_cont_head_div">
-            <div className="kypageDiv_cont_img_div">
-              <img
-                src="/img/verification_svg1.svg"
-                alt=""
-                className="kypageDiv_cont_img"
-              />
-            </div>
-            <div className="kypageDiv_cont_head">Verify Your Identity</div>
-            <div className="kypageDiv_cont_para">
-              Please submit the following requirements to process the
-              application.
-            </div>
+      <>
+        <div className="kypageDiv_cont_div_btn">
+          <div className="kypageDiv_cont_div_btn_func_btn" onClick={prev}>
+            <ArrowBackIcon className="kypageDiv_cont_div_btn_func_btn_icon" />
+            Home
           </div>
-          <div className="kypageDiv_cont_body_start_verify">
-            <div className="kypageDiv_cont_body_start_verify_cont1">
-              <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
-              <div className="kypageDiv_cont_body_start_verify_cont1_body2">
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
-                  Take a selfie of yourself
+          Verify
+        </div>
+        <div className="kypageDiv_cont">
+          <div className="kypageDiv_cont_body_conts">
+            <div className="kypageDiv_cont_head_div">
+              <div className="kypageDiv_cont_img_div">
+                <img
+                  src="/img/verification_svg1.svg"
+                  alt=""
+                  className="kypageDiv_cont_img"
+                />
+              </div>
+              <div className="kypageDiv_cont_head">Verify Your Identity</div>
+              <div className="kypageDiv_cont_para">
+                Please submit the following requirements to process the
+                application.
+              </div>
+            </div>
+            <div className="kypageDiv_cont_body_start_verify">
+              <div className="kypageDiv_cont_body_start_verify_cont1">
+                <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
+                <div className="kypageDiv_cont_body_start_verify_cont1_body2">
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
+                    Take a selfie of yourself
+                  </div>
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
+                    To match your face to your BVN
+                  </div>
                 </div>
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
-                  To match your face to your BVN
+              </div>
+              <div className="kypageDiv_cont_body_start_verify_cont1">
+                <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
+                <div className="kypageDiv_cont_body_start_verify_cont1_body2">
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
+                    Take a selfie of yourself
+                  </div>
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
+                    To match your face to your BVN
+                  </div>
+                </div>
+              </div>
+              <div className="kypageDiv_cont_body_start_verify_cont1">
+                <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
+                <div className="kypageDiv_cont_body_start_verify_cont1_body2">
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
+                    Take a selfie of yourself
+                  </div>
+                  <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
+                    To match your face to your BVN
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="kypageDiv_cont_body_start_verify_cont1">
-              <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
-              <div className="kypageDiv_cont_body_start_verify_cont1_body2">
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
-                  Take a selfie of yourself
-                </div>
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
-                  To match your face to your BVN
-                </div>
-              </div>
+          </div>
+          <div className="kypageDiv_cont_button_div">
+            <div className="kypageDiv_cont_button_div_txt">
+              Your infos will be encrypted and stored securely.
             </div>
-            <div className="kypageDiv_cont_body_start_verify_cont1">
-              <FaceIcon className="kypageDiv_cont_body_start_verify_cont1_icon" />
-              <div className="kypageDiv_cont_body_start_verify_cont1_body2">
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_title">
-                  Take a selfie of yourself
-                </div>
-                <div className="kypageDiv_cont_body_start_verify_cont1_body2_para">
-                  To match your face to your BVN
-                </div>
-              </div>
-            </div>
+            <button
+              className="kypageDiv_cont_button_div_btn"
+              onClick={startVerify}
+            >
+              Get Started
+            </button>
           </div>
         </div>
-        <div className="kypageDiv_cont_button_div">
-          <div className="kypageDiv_cont_button_div_txt">
-            Your infos will be encrypted and stored securely.
-          </div>
-          <button
-            className="kypageDiv_cont_button_div_btn"
-            onClick={startVerify}
-          >
-            Get Started
-          </button>
-        </div>
-      </div>
+      </>
     </div>
   );
 };
@@ -203,82 +258,89 @@ const KycBvnComp = ({ nextStep1, prevStep }) => {
 
   return (
     <div className="kypageDiv_cont_div">
-      <div className="kypageDiv_cont_div_btn">
-        <div className="kypageDiv_cont_div_btn_func_btn" onClick={prevStep}>
-          <ArrowBackIcon className="kypageDiv_cont_div_btn_func_btn_icon" />
-          Back
+      <>
+        <div className="kypageDiv_cont_div_btn">
+          <div className="kypageDiv_cont_div_btn_func_btn" onClick={prevStep}>
+            <ArrowBackIcon className="kypageDiv_cont_div_btn_func_btn_icon" />
+            Back
+          </div>
+          Step1/3
         </div>
-        Step1/3
-      </div>
-      <div className="kypageDiv_cont">
-        <div className="kypageDiv_cont_body_conts">
-          <div className="kypageDiv_cont_head_div">
-            <div className="kypageDiv_cont_head">Please provide your BVN</div>
-            <div className="kypageDiv_cont_para">
-              If you don't have your BVN yo can request from your bank
-              instituition, or dial the code below using the phone nuber
-              registered with your BVN.
-            </div>
-          </div>
-          <div className="kypageDiv_cont_body">
-            <div className="kypageDiv_cont_body_email_input_code">*565*0#</div>
-            <div className="kypageDiv_cont_body_email_input_div">
-              <input
-                type="number"
-                placeholder={"Enter BVN here"}
-                className="kypageDiv_cont_body_email_input2"
-                onChange={(e) => {
-                  const { value } = e.target;
-                  if (payload.bvnNumber.length >= 11) return;
-
-                  dispatch(setPayload({ ...payload, bvnNumber: value }));
-                }}
-              />
-            </div>
-          </div>
-          <div className="kypageDiv_cont_body_start_verify">
-            <div className="kypageDiv_cont_body_start_verify_title">
-              Why we need your BVN?
-            </div>
-            <div className="kypageDiv_cont_body_start_verify_answers_conts">
-              <div className="kypageDiv_cont_body_start_verify_answers_conts_title">
-                We use your BVN to verify the following
+        <div className="kypageDiv_cont">
+          <div className="kypageDiv_cont_body_conts">
+            <div className="kypageDiv_cont_head_div">
+              <div className="kypageDiv_cont_head">Please provide your BVN</div>
+              <div className="kypageDiv_cont_para">
+                If you don't have your BVN yo can request from your bank
+                instituition, or dial the code below using the phone nuber
+                registered with your BVN.
               </div>
-              <div className="kypageDiv_cont_body_start_verify_answers_cont1">
-                <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
+            </div>
+            <div className="kypageDiv_cont_body">
+              <div className="kypageDiv_cont_body_email_input_code">
+                *565*0#
+              </div>
+              <div className="kypageDiv_cont_body_email_input_div">
+                <input
+                  type="number"
+                  placeholder={"Enter BVN here"}
+                  className="kypageDiv_cont_body_email_input2"
+                  onChange={(e) => {
+                    const { value } = e.target;
+                    if (payload.bvnNumber.length >= 11) return;
 
-                <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
-                  Full name
+                    dispatch(setPayload({ ...payload, bvnNumber: value }));
+                  }}
+                />
+              </div>
+            </div>
+            <div className="kypageDiv_cont_body_start_verify">
+              <div className="kypageDiv_cont_body_start_verify_title">
+                Why we need your BVN?
+              </div>
+              <div className="kypageDiv_cont_body_start_verify_answers_conts">
+                <div className="kypageDiv_cont_body_start_verify_answers_conts_title">
+                  We use your BVN to verify the following
+                </div>
+                <div className="kypageDiv_cont_body_start_verify_answers_cont1">
+                  <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
+
+                  <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
+                    Full name
+                  </div>
+                </div>
+                <div className="kypageDiv_cont_body_start_verify_answers_cont1">
+                  <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
+
+                  <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
+                    Phone Number
+                  </div>
+                </div>
+                <div className="kypageDiv_cont_body_start_verify_answers_cont1">
+                  <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
+
+                  <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
+                    Date of Birth
+                  </div>
                 </div>
               </div>
-              <div className="kypageDiv_cont_body_start_verify_answers_cont1">
-                <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
-
-                <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
-                  Phone Number
-                </div>
-              </div>
-              <div className="kypageDiv_cont_body_start_verify_answers_cont1">
-                <VerifiedIcon className="kypageDiv_cont_body_start_verify_answers_cont1_icon" />
-
-                <div className="kypageDiv_cont_body_start_verify_answers_cont1_txt">
-                  Date of Birth
-                </div>
-              </div>
             </div>
           </div>
+          <div
+            className="kypageDiv_cont_button_div"
+            onClick={() => {
+              alert(JSON.stringify(payload));
+            }}
+          >
+            <button
+              className="kypageDiv_cont_button_div_btn"
+              onClick={nextStep1}
+            >
+              Next Step
+            </button>
+          </div>
         </div>
-        <div
-          className="kypageDiv_cont_button_div"
-          onClick={() => {
-            alert(JSON.stringify(payload));
-          }}
-        >
-          <button className="kypageDiv_cont_button_div_btn" onClick={nextStep1}>
-            Next Step
-          </button>
-        </div>
-      </div>
+      </>
     </div>
   );
 };
@@ -396,11 +458,13 @@ const KycAddressComp = ({ nextStep2, prevStep }) => {
   );
 };
 const KycFacialComp = ({ submitVerify, prevStep }) => {
+  const navigate = useNavigate();
   const { payload } = useSelector((state) => state.kyc);
   const webcamRef = useRef(null);
   const dispatch = useDispatch();
   const [capturedImage, setCapturedImage] = useState(null);
   const [openCamera, setOpenCamera] = useState(false);
+  const [processing, setProcessing] = useState(false);
 
   const captureImage = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -488,31 +552,54 @@ const KycFacialComp = ({ submitVerify, prevStep }) => {
         </div>
 
         <div className="kypageDiv_cont_button_div">
-          <button
-            className="kypageDiv_cont_button_div_btn"
-            onClick={async () => {
-              const file = dataUrlToFile(payload.image, "file.jpg");
-              let data = new FormData();
-              data.append("image_file", file);
-              const response = await UPLOAD_IMAGE(data);
-              console.log(response);
+          {processing ? (
+            <p>loading ...</p>
+          ) : (
+            <button
+              className="kypageDiv_cont_button_div_btn"
+              onClick={async () => {
+                setProcessing(true);
+                const file = dataUrlToFile(payload.image, "file.jpg");
+                let data = new FormData();
+                data.append("image_file", file);
+                const response = await UPLOAD_IMAGE(data);
+                console.log(response);
 
-              if (!response.success || response?.success === false) {
-                return;
-              }
+                if (!response.success || response?.success === false) {
+                  return;
+                }
 
-              let new_payload = payload;
-              new_payload = { ...new_payload, image: response.data.image_name };
-              console.log(new_payload);
-              const res = await UPLOAD_LEVEL_2_KYC(new_payload);
+                let new_payload = payload;
+                new_payload = {
+                  ...new_payload,
+                  image: response.data.image_name,
+                };
+                console.log(new_payload);
+                const res = await UPLOAD_LEVEL_2_KYC(new_payload);
+                console.log(res);
 
-              // console.log(res);
-            }}
-          >
-            Submit Verification
-          </button>
+                setProcessing(false);
+                if (res.success) {
+                  toast.success("Information Submitted Successfully");
+                  setTimeout(() => {
+                    navigate(0);
+                  }, 2000);
+                  return;
+                }
+
+                if (typeof res?.data?.errorMessage === "string") {
+                  toast.error(res.data.errorMessage);
+                  return;
+                }
+                toast.success("An error Occured!!!");
+              }}
+            >
+              Submit Verification
+            </button>
+          )}
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
